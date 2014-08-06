dd5fc92005c5bb70f45e3aa5fa31bba94137ae32
Shindeok Kang <shindeok.kang@dena.com>
2014-08-06 18:14:06 +0900

    Build test

---
+++ dist/scripts/main.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(a){console.log(a.currentTarget+"."+a.type)},!1),window.addEventListener("load",function(a){console.log(a.currentTarget+"."+a.type)},!1),window.addEventListener("orientationchange",function(a){console.log(a.currentTarget+"."+a.type),(90===window.orientation||-90===window.orientation)&&(document.body.scrollTop=0)},!1);
