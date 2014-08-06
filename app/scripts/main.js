032dba845ca36dc46b34e46cf94aa6a0f087a41e
Shindeok Kang <shindeok.kang@dena.com>
2014-08-06 17:40:00 +0900

    Update template files

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,36 @@
+//------------------------------------------------------------------------------
+//
+//  Constants
+//
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+//
+//  Variables
+//
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+//
+//  Event handlers
+//
+//------------------------------------------------------------------------------
+
 document.addEventListener('DOMContentLoaded', function(event){
   console.log(event.currentTarget + '.' +  event.type);
 }, false);
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function(event){
   console.log(event.currentTarget + '.' +  event.type);
+}, false);
+
+
+window.addEventListener('orientationchange', function(event) {
+  console.log(event.currentTarget + '.' +  event.type);
+  
+  // Workarounds for iOS Safari landscape bug
+  if(90 === window.orientation || -90 === window.orientation) {
+    document.body.scrollTop = 0;
+  }
+  
 }, false);
