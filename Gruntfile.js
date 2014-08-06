4093c5730f53b6d6ee37514449c04494bf5b6903
Shindeok Kang <shindeok.kang@dena.com>
2014-08-06 17:40:43 +0900

    Update grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,22 @@
 'use strict';
 
+//------------------------------------------------------------------------------
+//
+//  Constants
+//
+//------------------------------------------------------------------------------
+
 var CONNECT_HOST = '0.0.0.0';
 var CONNECT_PORT = 9001;
 
 var LIVERELOAD_HOST = '0.0.0.0';
 var LIVERELOAD_PORT = 35731;
+
+//------------------------------------------------------------------------------
+//
+//  Functions
+//
+//------------------------------------------------------------------------------
 
 var lrSnippet = require('connect-livereload')({ 
   hostname: LIVERELOAD_HOST,
@@ -15,8 +27,19 @@ var mountFolder = function (connect, dir) {
   return connect.static(require('path').resolve(dir));
 };
 
+//------------------------------------------------------------------------------
+//
+//  Initialize
+//
+//------------------------------------------------------------------------------
+
 module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
+  require('time-grunt')(grunt);
+
+//--------------------------------------
+//  Grunt init config
+//--------------------------------------
 
   // Project configuration.
   grunt.initConfig({
@@ -84,7 +107,9 @@ module.exports = function(grunt) {
           dest: 'dist',
           src: [
             'images/**/*.{png,jpg,gif}',
-            '**/*.html'
+            '**/*.html',
+            '!styleguide/**/*.html',
+            '!styleguide-template/**/*.html'
           ]
         }]
       }
@@ -131,8 +156,11 @@ module.exports = function(grunt) {
     },
 
     usemin: {
-      html: 'dist/**/*.html',
-      css: 'dist/styles/**/*.css'
+      html: [
+        'dist/**/*.html',
+        '!dist/bower_components/**/*.html'
+      ],
+      // css: 'dist/styles/**/*.css' // Replacing css image url not needed. (Compass doing cash busting)
     },
 
     htmlmin: {
@@ -165,10 +193,14 @@ module.exports = function(grunt) {
         command: 'sass --compass -l -t expanded --watch app/styles/sass:app/styles'
       },
       styleguide: {
-        command: 'mkdir -p styleguide; node_modules/kss/bin/kss-node app/styles/sass styleguide -t styleguide-template --css app/styles/main.css'
+        command: 'mkdir -p app/styleguide; node_modules/kss/bin/kss-node app/styles/sass app/styleguide -t app/styleguide-template --css app/styles/main.css'
       }
     }
   });
+
+//--------------------------------------
+//  Grunt tasks
+//--------------------------------------
 
   // Server task.
   grunt.registerTask('serve', 'Launch local web server and enable live-reloading.', function(target) {
@@ -181,9 +213,12 @@ module.exports = function(grunt) {
     } else {
       tasks = [
         'connect:livereload', 
+        'shell:sassWatch',
         'watch'
       ];
     }
+    
+    grunt.config('shell.sassWatch.options.async', true);
     
     grunt.task.run(tasks);
   });
@@ -191,13 +226,14 @@ module.exports = function(grunt) {
   // Build task.
   grunt.registerTask('build', 'Minify CSS/JS/HTML and revisioning all static files.', function() {
     var tasks = [
+      'shell:sassUpdate',
       'clean:dist',
       'copy:build',
       'useminPrepare',
       'concat',
       'cssmin',
       'filerev:images',
-      'usemin:css',
+      // 'usemin:css', // Replacing css image url not needed. (Compass doing cash busting)
       'uglify',
       'filerev:styles',
       'filerev:scripts',
